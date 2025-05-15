9869f1901834bc612abb3abf3920b1ed09fda10c
Roshan Gangurde <rgangurde1993@gmail.com>
2025-05-15 16:35:43 +0530

    server.js fix issue for render

--- backend/server.js
+++ backend/server.js
@@ -34,4 +34,4 @@ app.get("/", (req, res) => {
 });
 
 
-app.listen(PORT, () => console.log(` Server running on http://localhost:${PORT}`));
+app.listen(PORT, () => console.log(` Server running on ${PORT}`));
